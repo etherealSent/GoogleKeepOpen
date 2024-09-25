74cc430b772b0ad331be9f5b4be0ef42316fb3be
godnesty <ethereal.sent@gmail.com>
2024-09-25 19:14:12 +0300

    Dark Theme added

--- app/src/main/java/com/example/petproject/ui/theme/Color.kt
+++ app/src/main/java/com/example/petproject/ui/theme/Color.kt
@@ -9,3 +9,7 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val DarkSurface = Color(0xFF0E1418)
+val DarkSurfaceContainer = Color(0xFF1A2024)
+val DarkOnSurface = Color(0xFFBAC0C7)
--- app/src/main/java/com/example/petproject/ui/theme/Theme.kt
+++ app/src/main/java/com/example/petproject/ui/theme/Theme.kt
@@ -14,7 +14,12 @@ import androidx.compose.ui.platform.LocalContext
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
     secondary = PurpleGrey80,
-    tertiary = Pink80
+    tertiary = Pink80,
+    // background area
+    surface = DarkSurface,
+    // navigation area
+    surfaceContainer = DarkSurfaceContainer,
+    onSurface = DarkOnSurface
 )
 
 private val LightColorScheme = lightColorScheme(
